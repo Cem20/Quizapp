fb5bb90590be619450bd026c86a776a9c4407293
Cem20 <cinar.cem@web.de>
2023-12-27 01:53:01 +0100

    Answers

---
+++ script.js
@@ -0,0 +1,74 @@
+let questions = [
+    {
+        "question":"Wer hat HTML erfunden?",
+        "answer_1": "Robbie Williamson",
+        "answer_2": "Lady Gaga",
+        "answer_3": "Tim Berners-Lee",
+        "answer_4": "Justin Bieber",
+        "right_answer": 3,
+    },
+    {
+        "question":"Welches Attribut kann man NICHT für Textarea verwenden?",
+        "answer_1": "readonly",
+        "answer_2": "max",
+        "answer_3": "from",
+        "answer_4": "spellcheck",
+        "right_answer": 1,
+    },
+    {
+        "question":"Wie wählst du alle Elemente vom Typ &lt;a&gt; mit dem attrribut title aus?",
+        "answer_1": "a[title]{...}",
+        "answer_2": "a > title {...}",
+        "answer_3": "a.title {...}",
+        "answer_4": "a-title {...}",
+        "right_answer": 1,
+    },
+    {
+        "question":"Wie definiert man in JavaScript eine Variable?",
+        "answer_1": "let 100 = rate;",
+        "answer_2": "100 = let rate;",
+        "answer_3": "rate = 100",
+        "answer_4": "let rate = 100",
+        "right_answer": 4,
+    },
+    {
+        "question":"Wie viele Planeten hat unser Sonnensystem?",
+        "answer_1": "7",
+        "answer_2": "8",
+        "answer_3": "9",
+        "answer_4": "10",
+        "right_answer": 2,
+    },
+    {
+        "question":"Welcher Planet ist der größte in unserem Sonnesystem?",
+        "answer_1": "Jupiter",
+        "answer_2": "Mars",
+        "answer_3": "Saturn",
+        "answer_4": "Merkur",
+        "right_answer": 1,
+    },
+    {
+        "question":"Wer hat den Apple 1 entwicklet?",
+        "answer_1": "Steve Jobs",
+        "answer_2": "Steve Wozniak",
+        "answer_3": "Ronald Wayne",
+        "answer_4": " Daniel Kottke",
+        "right_answer": 2,
+    }
+];
+
+let currentQuestion = 0;
+
+
+function init(){
+    showQuestion();
+}
+
+function showQuestion(){
+    let question = questions[currentQuestion];
+    document.getElementById('Question').innerHTML = `${question['question']}`;
+    document.getElementById('answer1').innerHTML = `${question['answer_1']}`;
+    document.getElementById('answer2').innerHTML = `${question['answer_2']}`;
+    document.getElementById('answer3').innerHTML = `${question['answer_3']}`;
+    document.getElementById('answer4').innerHTML = `${question['answer_4']}`;
+}
